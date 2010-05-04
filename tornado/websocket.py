9ea5f8a277db96557ae7867375c7db4d8c5f633a
Dan Loewenherz <dloewenherz@gmail.com>
2010-05-04 15:59:22 -0700

    websockets: continue listening for messages after parsing completion
    The prior implementation stops listening for messages after the first
    message is received.

--- tornado/websocket.py
+++ tornado/websocket.py
@@ -127,6 +127,7 @@ class WebSocketHandler(tornado.web.RequestHandler):
 
     def _on_end_delimiter(self, callback, frame):
         callback(frame[:-1].decode("utf-8", "replace"))
+        self.receive_message(callback)
 
     def _not_supported(self, *args, **kwargs):
         raise Exception("Method not supported for Web Sockets")
