a785a128d5cf79480234c970e06f68f8de97580b
Ben Darnell <ben@bendarnell.com>
2011-02-23 14:22:23 -0800

    Add a utility function to import an object by name.

--- tornado/test/runtests.py
+++ tornado/test/runtests.py
@@ -4,6 +4,7 @@ import unittest
 TEST_MODULES = [
     'tornado.httputil.doctests',
     'tornado.iostream.doctests',
+    'tornado.util.doctests',
     'tornado.test.escape_test',
     'tornado.test.httpserver_test',
     'tornado.test.ioloop_test',
---
+++ tornado/util.py
@@ -0,0 +1,20 @@
+"""Miscellaneous utility functions."""
+
+def import_object(name):
+    """Imports an object by name.
+
+    import_object('x.y.z') is equivalent to 'from x.y import z'.
+
+    >>> import tornado.escape
+    >>> import_object('tornado.escape') is tornado.escape
+    True
+    >>> import_object('tornado.escape.utf8') is tornado.escape.utf8
+    True
+    """
+    parts = name.split('.')
+    obj = __import__('.'.join(parts[:-1]), None, None, [parts[-1]], 0)
+    return getattr(obj, parts[-1])
+
+def doctests():
+    import doctest
+    return doctest.DocTestSuite()
