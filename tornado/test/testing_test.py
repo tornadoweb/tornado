6f0a8a752d03ea20057ac57d958825de688c6555
Brian Beach <coder@beachfamily.net>
2011-03-14 11:34:19 -0400

    Make AsyncTestCase call tearDown in super class.
    The setUp method was calling super, but tearDown wasn't.  Now they
    both are.
    
    This change includes a new unit test case to verify the fix.

--- tornado/test/testing_test.py
+++ tornado/test/testing_test.py
@@ -11,5 +11,33 @@ class AsyncTestCaseTest(AsyncTestCase, LogTrapTestCase):
         except ZeroDivisionError:
             pass
 
+class SetUpTearDownTest(unittest.TestCase):
+    def test_set_up_tear_down(self):
+        """
+        This test makes sure that AsyncTestCase calls super methods for
+        setUp and tearDown.
+
+        InheritBoth is a subclass of both AsyncTestCase and
+        SetUpTearDown, with the ordering so that the super of
+        AsyncTestCase will be SetUpTearDown.
+        """
+        events = []
+        result = unittest.TestResult()
+
+        class SetUpTearDown(unittest.TestCase):
+            def setUp(self):
+                events.append('setUp')
+
+            def tearDown(self):
+                events.append('tearDown')
+
+        class InheritBoth(AsyncTestCase, SetUpTearDown):
+            def test(self):
+                events.append('test')
+
+        InheritBoth('test').run(result)
+        expected = ['setUp', 'test', 'tearDown']
+        self.assertEqual(expected, events)
+
 if __name__ == '__main__':
-    unittest.main
+    unittest.main()
