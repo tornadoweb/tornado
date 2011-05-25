a0291bb0e25a1b26a42e724e607f7ce9a729893c
nixon <nixon@shun.org>
2011-05-24 23:37:48 -0500

    move test specifics out of setUp

--- tornado/test/auth_test.py
+++ tornado/test/auth_test.py
@@ -8,53 +8,85 @@ import urlparse
 
 class TestMixinQueryParts(unittest.TestCase):
 
-    def setUp(self):
-        self.fb = FacebookGraphMixin()
-        self.oa2 = OAuth2Mixin()
-        self.oa2._OAUTH_ACCESS_TOKEN_URL = "https://localhost/access_token"
-        self.oa2._OAUTH_AUTHORIZE_URL = "https://localhost/authorize"
+    def _mock_redirect(self,url):
+        self._redirected_to = url
+
+    def test_oa2_q_oauth_request_token_url_has_query_parts(self):
+        self.oa2_q = OAuth2Mixin()
+        self.oa2_q._OAUTH_ACCESS_TOKEN_URL = "https://localhost/access_token?x&"
+
         self.expected_tok_args = [
                 'client_secret',
                 'code',
                 'client_id',
                 'redirect_uri'
                 ]
-        self.expected_auth_args = [
-                'client_id',
-                'redirect_uri'
-                ]
-        self.oa2.redirect = self._mock_redirect
-        self.fb.redirect = self._mock_redirect
 
-        self.oa2_q = OAuth2Mixin()
-        self.oa2_q._OAUTH_ACCESS_TOKEN_URL = "https://localhost/access_token?x&"
-        self.oa2_q._OAUTH_AUTHORIZE_URL = "https://localhost/authorize?x&"
-        self.oa2_q.redirect = self._mock_redirect
-
-    def _mock_redirect(self,url):
-        self._redirected_to = url
-
-    def test_oa2_q_oauth_request_token_url_has_query_parts(self):
         url = self.oa2_q._oauth_request_token_url()
         self._test_query_parts(url, self.expected_tok_args)
 
     def test_oa2_q_authorize_redirect_has_query_parts(self):
+        self.oa2_q = OAuth2Mixin()
+        self.oa2_q._OAUTH_AUTHORIZE_URL = "https://localhost/authorize?x&"
+        self.oa2_q.redirect = self._mock_redirect
+
+        self.expected_auth_args = [
+                'client_id',
+                'redirect_uri'
+                ]
+
         self.oa2_q.authorize_redirect()
         self._test_query_parts(self._redirected_to, self.expected_auth_args)
 
     def test_oa2_oauth_request_token_url_has_query_parts(self):
+        self.oa2 = OAuth2Mixin()
+        self.oa2._OAUTH_ACCESS_TOKEN_URL = "https://localhost/access_token"
+
+        self.expected_tok_args = [
+                'client_secret',
+                'code',
+                'client_id',
+                'redirect_uri'
+                ]
+
         url = self.oa2._oauth_request_token_url()
         self._test_query_parts(url, self.expected_tok_args)
 
     def test_oa2_authorize_redirect_has_query_parts(self):
+        self.oa2 = OAuth2Mixin()
+        self.oa2._OAUTH_AUTHORIZE_URL = "https://localhost/authorize"
+        self.oa2.redirect = self._mock_redirect
+
+        self.expected_auth_args = [
+                'client_id',
+                'redirect_uri'
+                ]
+
         self.oa2.authorize_redirect()
         self._test_query_parts(self._redirected_to, self.expected_auth_args)
 
     def test_fb_oauth_request_token_url_has_query_parts(self):
+        self.fb = FacebookGraphMixin()
+
+        self.expected_tok_args = [
+                'client_secret',
+                'code',
+                'client_id',
+                'redirect_uri'
+                ]
+
         url = self.fb._oauth_request_token_url()
         self._test_query_parts(url, self.expected_tok_args)
 
     def test_fb_authorize_redirect_has_query_parts(self):
+        self.fb = FacebookGraphMixin()
+        self.fb.redirect = self._mock_redirect
+
+        self.expected_auth_args = [
+                'client_id',
+                'redirect_uri'
+                ]
+
         self.fb.authorize_redirect()
         self._test_query_parts(self._redirected_to, self.expected_auth_args)
 
