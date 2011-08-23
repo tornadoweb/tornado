f5fe19758444694f4654827bb4a8a1ace7dee1fd
William Chau <william@contextlogic.com>
2011-08-23 12:16:18 -0700

    Rename branch with contextlogic tag.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ if "linux" in sys.platform.lower() and not python_26:
     extensions.append(distutils.core.Extension(
         "tornado.epoll", ["tornado/epoll.c"]))
 
-version = "2.0git"
+version = "2.0git-cl"
 
 if major >= 3:
     import setuptools  # setuptools is required for use_2to3
--- tornado/__init__.py
+++ tornado/__init__.py
@@ -16,5 +16,5 @@
 
 """The Tornado web server and tools."""
 
-version = "2.0git"
+version = "2.0git-cl"
 version_info = (2, 0, 0)
