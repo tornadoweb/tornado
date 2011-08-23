e041d40d1d4ea288f9c62fb07fe99589dffacc97
Jeeyoung Kim <jeeyoung@contextlogic.com>
2011-08-23 10:51:42 -0700

    Revert "Ignore connection closed EPOLL events and continue reading"
    This reverts commit 118a79b913584ed11767157173fe3ec74bea1a6e.

--- tornado/iostream.py
+++ tornado/iostream.py
@@ -79,12 +79,13 @@ class IOStream(object):
 
     """
     def __init__(self, socket, io_loop=None, max_buffer_size=104857600,
-                 read_chunk_size=4096):
+                 read_chunk_size=4096, ignore_hangup=True):
         self.socket = socket
         self.socket.setblocking(False)
         self.io_loop = io_loop or ioloop.IOLoop.instance()
         self.max_buffer_size = max_buffer_size
         self.read_chunk_size = read_chunk_size
+        self.ignore_hangup = ignore_hangup
         self._read_buffer = collections.deque()
         self._write_buffer = collections.deque()
         self._read_buffer_size = 0
@@ -100,7 +101,8 @@ class IOStream(object):
         self._connecting = False
         self._state = None
         self._pending_callbacks = 0
-        self._conn_closed = False
+        self._error_flags = ioloop.IOLoop.ERROR & ~ioloop.IOLoop._EPOLLRDHUP if\
+            ignore_hangup else ioloop.IOLoop.ERROR
 
     def connect(self, address, callback=None):
         """Connects the socket to a remote address without blocking.
@@ -250,17 +252,13 @@ class IOStream(object):
                 self._handle_write()
             if not self.socket:
                 return
-            if events & self.io_loop.ERROR:
+            if events & self._error_flags:
                 # We may have queued up a user callback in _handle_read or
                 # _handle_write, so don't close the IOStream until those
                 # callbacks have had a chance to run.
                 self.io_loop.add_callback(self.close)
                 return
-            if events & self.io_loop.CONN_CLOSED:
-                self.io_loop.remove_handler(self.socket.fileno())
-                self._conn_closed = True
-                return
-            state = self.io_loop.ERROR | self.io_loop.CONN_CLOSED
+            state = self._error_flags
             if self.reading():
                 state |= self.io_loop.READ
             if self.writing():
@@ -474,7 +472,7 @@ class IOStream(object):
             raise IOError("Stream is closed")
 
     def _maybe_add_error_listener(self):
-        if (self._state is None or self._conn_closed) and self._pending_callbacks == 0:
+        if self._state is None and self._pending_callbacks == 0:
             if self.socket is None:
                 cb = self._close_callback
                 if cb is not None:
@@ -508,7 +506,7 @@ class IOStream(object):
             # connection has been closed, so there can be no future events
             return
         if self._state is None:
-            self._state = self.io_loop.ERROR | self.io_loop.CONN_CLOSED | state
+            self._state = self._error_flags | state
             with stack_context.NullContext():
                 self.io_loop.add_handler(
                     self.socket.fileno(), self._handle_events, self._state)
