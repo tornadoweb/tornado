93a0dc821c6341bf9f315275341e9b71fecabd4b
Ben Darnell <ben@bendarnell.com>
2011-09-04 12:52:43 -0700

    Fix error in docs

--- tornado/gen.py
+++ tornado/gen.py
@@ -23,7 +23,7 @@ could be written with ``gen`` as::
         @gen.engine
         def get(self):
             http_client = AsyncHTTPClient()
-            response = yield gen.Task(http_client.fetch("http://example.com"))
+            response = yield gen.Task(http_client.fetch, "http://example.com")
             do_something_with_response(response)
             self.render("template.html")
 
