326523bebae53c068c80d6b5f3a84db937820a75
Ben Darnell <ben@bendarnell.com>
2011-10-25 10:50:22 -0700

    Add return statements missing from HEAD/204 fix

--- tornado/simple_httpclient.py
+++ tornado/simple_httpclient.py
@@ -329,12 +329,14 @@ class _HTTPConnection(object):
             # HEAD requests never have content, even though they may have
             # content-length headers
             self._on_body(b(""))
+            return
         if 100 <= self.code < 200 or self.code in (204, 304):
             # These response codes never have bodies
             # http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.3
             assert "Transfer-Encoding" not in self.headers
             assert content_length in (None, 0)
             self._on_body(b(""))
+            return
 
         if (self.request.use_gzip and
             self.headers.get("Content-Encoding") == "gzip"):
