3e14e9b46ff0b469847575e9ff0159863dceb984
Shotaro Emori <emorins@gmail.com>
2011-12-05 18:48:37 +0900

    Added @authenticated_to_login

--- tornado/web.py
+++ tornado/web.py
@@ -1665,18 +1665,25 @@ def authenticated(method):
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
         if not self.current_user:
-            if self.request.method in ("GET", "HEAD"):
-                url = self.get_login_url()
-                if "?" not in url:
-                    if urlparse.urlsplit(url).scheme:
-                        # if login url is absolute, make next absolute too
-                        next_url = self.request.full_url()
-                    else:
-                        next_url = self.request.uri
-                    url += "?" + urllib.urlencode(dict(next=next_url))
-                self.redirect(url)
-                return
             raise HTTPError(403)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def authenticated_to_login(method):
+    """Decorate methods with this to require that the user be logged in."""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        if not self.current_user:
+            url = self.get_login_url()
+            if "?" not in url:
+                if urlparse.urlsplit(url).scheme:
+                    # if login url is absolute, make next absolute too
+                    next_url = self.request.full_url()
+                else:
+                    next_url = self.request.uri
+                url += "?" + urllib.urlencode(dict(next=next_url))
+            self.redirect(url)
+            return
         return method(self, *args, **kwargs)
     return wrapper
 
