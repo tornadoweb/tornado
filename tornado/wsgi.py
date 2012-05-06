c3ed68a31d66a203aff2a1ebd8a9baa68d526f3e
Casey Muller <github@nullterminated.com>
2012-05-06 11:18:29 -0700

    Fix setting cookies under WSGI, broken by the rename from _new_cookies to _new_cookie in https://github.com/facebook/tornado/commit/70ea7a5a6ca956b613168c9d123ee7cc951054bf

--- tornado/wsgi.py
+++ tornado/wsgi.py
@@ -96,8 +96,8 @@ class WSGIApplication(web.Application):
         status = str(handler._status_code) + " " + \
             httplib.responses[handler._status_code]
         headers = handler._headers.items()
-        for cookie_dict in getattr(handler, "_new_cookies", []):
-            for cookie in cookie_dict.values():
+        if hasattr(handler, "_new_cookie"):
+            for cookie in handler._new_cookie.values():
                 headers.append(("Set-Cookie", cookie.OutputString(None)))
         start_response(status,
                        [(native_str(k), native_str(v)) for (k, v) in headers])
