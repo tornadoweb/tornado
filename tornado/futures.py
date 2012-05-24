46ca3260b03a0a83843a3e9ccbf528e82fc31a50
Jason Choy <jjwchoy@gmail.com>
2012-05-24 08:13:02 +0100

    Added futures compatible with concurrent.futures

---
+++ tornado/futures.py
@@ -0,0 +1,57 @@
+import tornado.ioloop
+import tornado.gen
+
+from concurrent.futures import _base
+
+class _WorkItem(object):
+    def __init__(self, future, fn, args, kwargs):
+        self.future = future
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self):
+        if not self.future.set_running_or_notify_cancel():
+            return
+
+        self.kwargs['callback'] = self
+        self.fn(*self.args, **self.kwargs)
+
+
+    def __call__(self, *args, **kwargs):
+        self.future.set_result((args, kwargs))
+
+
+
+class TornadoExecutor(_base.Executor):
+    def __init__(self):
+        self._shutdown = False
+
+    def submit(self, fn, *args, **kwargs):
+        if self._shutdown:
+            raise RuntimeError('cannot schedule new futures after shutdown')
+
+        f = _base.Future()
+        w = _WorkItem(f, fn, args, kwargs)
+        tornado.ioloop.IOLoop.instance().add_callback(w.run)
+        return f
+
+    def shutdown(self, wait=True):
+        # TODO
+        raise NotImplemented
+
+class FutureWait(tornado.gen.YieldPoint):
+    def __init__(self, future):
+        self.future = future
+
+    def start(self, runner):
+        self.runner = runner
+        self.key = object()
+        runner.register_callback(self.key)
+        self.future.add_done_callback(runner.result_callback(self.key))
+
+    def is_ready(self):
+        return self.runner.is_ready(self.key)
+
+    def get_result(self):
+        return self.runner.pop_result(self.key)
