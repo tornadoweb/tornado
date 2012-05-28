5cd350ace0e248d163c9babec8932ec6f4351428
Ben Darnell <ben@bendarnell.com>
2012-05-27 22:10:03 -0700

    Set version number to 2.3rc1

--- tornado/__init__.py
+++ tornado/__init__.py
@@ -25,5 +25,5 @@ from __future__ import absolute_import, division, with_statement
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate (after the base version number
 # has been incremented)
-version = "2.2.post1"
-version_info = (2, 2, 0, 1)
+version = "2.3.rc1"
+version_info = (2, 3, 0, -1)
