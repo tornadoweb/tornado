ed7030d280962a2ba5a9861689eecd6edbabfd94
Ben Darnell <ben@bendarnell.com>
2012-06-29 00:53:22 -0700

    Disallow multiple calls to add_handler for the same fd in IOLoop.
    This was already true for epoll; this change adds a check for
    kqueue and select implementations.

--- tornado/test/ioloop_test.py
+++ tornado/test/ioloop_test.py
@@ -3,10 +3,13 @@
 
 from __future__ import absolute_import, division, with_statement
 import datetime
-import unittest
+import socket
 import time
+import unittest
 
-from tornado.testing import AsyncTestCase, LogTrapTestCase
+from tornado.ioloop import IOLoop
+from tornado.netutil import bind_sockets
+from tornado.testing import AsyncTestCase, LogTrapTestCase, get_unused_port
 
 
 class TestIOLoop(AsyncTestCase, LogTrapTestCase):
@@ -30,6 +33,21 @@ class TestIOLoop(AsyncTestCase, LogTrapTestCase):
     def test_add_timeout_timedelta(self):
         self.io_loop.add_timeout(datetime.timedelta(microseconds=1), self.stop)
         self.wait()
+
+    def test_multiple_add(self):
+        [sock] = bind_sockets(get_unused_port(), '127.0.0.1',
+                              family=socket.AF_INET)
+        try:
+            self.io_loop.add_handler(sock.fileno(), lambda fd, events: None,
+                                     IOLoop.READ)
+            # Attempting to add the same handler twice fails
+            # (with a platform-dependent exception)
+            self.assertRaises(Exception, self.io_loop.add_handler,
+                              sock.fileno(), lambda fd, events: None,
+                              IOLoop.READ)
+        finally:
+            sock.close()
+
 
 if __name__ == "__main__":
     unittest.main()
