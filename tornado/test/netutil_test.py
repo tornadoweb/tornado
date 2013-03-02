758513329280f035a592a1838dfc0d8eb81ea7cf
Ben Darnell <ben@bendarnell.com>
2013-03-02 14:55:10 -0500

    Fix twisted version check.

--- tornado/test/netutil_test.py
+++ tornado/test/netutil_test.py
@@ -65,7 +65,7 @@ class CaresResolverTest(AsyncTestCase, _ResolverTestMixin):
 
 
 @unittest.skipIf(twisted is None, "twisted module not present")
-@unittest.skipIf(twisted.__version__ < "12.1", "old version of twisted")
+@unittest.skipIf(getattr(twisted, '__version__', '0.0') < "12.1", "old version of twisted")
 class TwistedResolverTest(AsyncTestCase, _ResolverTestMixin):
     def setUp(self):
         super(TwistedResolverTest, self).setUp()
