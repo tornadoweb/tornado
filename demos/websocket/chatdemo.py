bc4917b65b25e26b388abc181895befa4cc86202
Ben Darnell <ben@bendarnell.com>
2013-03-03 12:23:28 -0500

    Use autoescape in websocket chat demo.

--- demos/websocket/chatdemo.py
+++ demos/websocket/chatdemo.py
@@ -43,7 +43,6 @@ class Application(tornado.web.Application):
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             xsrf_cookies=True,
-            autoescape=None,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 
