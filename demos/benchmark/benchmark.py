d02b031bc98b3d86b0bf3e8278edbcbb49c6d1a0
Ben Darnell <ben@bendarnell.com>
2013-04-07 13:07:23 -0400

    Add an ioloop command line flag to the benchmark script.

--- demos/benchmark/benchmark.py
+++ demos/benchmark/benchmark.py
@@ -39,6 +39,8 @@ define("quiet", type=bool, default=False)
 # --n=15000 for its JIT to reach full effectiveness
 define("num_runs", type=int, default=1)
 
+define("ioloop", type=str, default=None)
+
 class RootHandler(RequestHandler):
     def get(self):
         self.write("Hello, world")
@@ -51,6 +53,8 @@ def handle_sigchld(sig, frame):
 
 def main():
     parse_command_line()
+    if options.ioloop:
+        IOLoop.configure(options.ioloop)
     for i in xrange(options.num_runs):
         run()
 
