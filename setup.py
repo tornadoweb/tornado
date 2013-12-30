bf2cf0e2026d6057abe6aa1873c7fd9aa2417243
Ben Darnell <ben@bendarnell.com>
2013-12-30 14:17:57 -0500

    Add python 3.4 tag to setup.py

--- setup.py
+++ setup.py
@@ -166,6 +166,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         ],
