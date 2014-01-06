c2a25ac3975288c312ef7d630f65712fa8fbac49
Gavin M. Roy <gavinmroy@gmail.com>
2014-01-06 16:55:35 -0500

    Correct example variable, use consistant quoting

--- tornado/auth.py
+++ tornado/auth.py
@@ -958,21 +958,22 @@ class GoogleOAuth2Mixin(OAuth2Mixin):
 
         Example usage::
 
-            class GoogleOAuth2LoginHandler(LoginHandler, tornado.auth.GoogleOAuth2Mixin):
+            class GoogleOAuth2LoginHandler(LoginHandler,
+                                           tornado.auth.GoogleOAuth2Mixin):
                 @tornado.gen.coroutine
                 def get(self):
-                    if self.get_argument("code", False):
+                    if self.get_argument('code', False):
                         user = yield self.get_authenticated_user(
                             redirect_uri='http://your.site.com/auth/google',
-                            code=self.get_argument("code"))
+                            code=self.get_argument('code'))
                         # Save the user with e.g. set_secure_cookie
                     else:
                         yield self.authorize_redirect(
                             redirect_uri='http://your.site.com/auth/google',
-                            client_id=self.settings["google_consumer_key"],
-                            scope=['openid', 'email'],
+                            client_id=self.settings['google_oauth']['key'],
+                            scope=['profile', 'email'],
                             response_type='code',
-                            extra_params={"approval_prompt": "auto"})
+                            extra_params={'approval_prompt': 'auto'})
         """
         http = self.get_auth_http_client()
         body = urllib_parse.urlencode({
