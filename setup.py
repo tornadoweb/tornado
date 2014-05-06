989693186242844e3f94955a91d452b220320ad0
Ben Darnell <ben@bendarnell.com>
2014-05-05 22:28:23 -0400

    Set version number to 3.2.1

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ http://api.mongodb.org/python/current/installation.html#osx
 
 kwargs = {}
 
-version = "3.2"
+version = "3.2.1"
 
 with open('README.rst') as f:
     kwargs['long_description'] = f.read()
