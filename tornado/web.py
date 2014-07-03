65e744951f47db8db043bcb8c8278c0d2e4d230d
alvan <alvan@users.noreply.github.com>
2014-07-03 13:30:43 +0800

    Update web.py to implements default method head()
    see RFC2616: http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html

--- tornado/web.py
+++ tornado/web.py
@@ -206,7 +206,7 @@ class RequestHandler(object):
         return self.application.settings
 
     def head(self, *args, **kwargs):
-        raise HTTPError(405)
+        apply(self.get, args, kwargs)
 
     def get(self, *args, **kwargs):
         raise HTTPError(405)
