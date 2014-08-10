93ae079795f51e23d11a8f7d05fa48ea6bb19cbf
Ben Darnell <ben@bendarnell.com>
2014-08-10 14:02:39 -0400

    Correctly handle 204 response codes that do not include a content-length.
    If a server sends a 204 with no content-length we would wait for the server
    to close the connection instead of ending the request (any such servers
    are ignoring the "Connection: close" header we send, but apparently exist).
    
    Move some content-length logic from simple_httpclient.py to http1connection.py.
    
    Fix the client-side use of on_connection_close; this affects the
    handling of any HTTPInputException.
    
    This fixes regressions relative to Tornado 3.2.

--- tornado/websocket.py
+++ tornado/websocket.py
@@ -894,12 +894,12 @@ class WebSocketClientConnection(simple_httpclient._HTTPConnection):
             self.protocol.close(code, reason)
             self.protocol = None
 
-    def _on_close(self):
+    def on_connection_close(self):
         if not self.connect_future.done():
             self.connect_future.set_exception(StreamClosedError())
         self.on_message(None)
         self.tcp_client.close()
-        super(WebSocketClientConnection, self)._on_close()
+        super(WebSocketClientConnection, self).on_connection_close()
 
     def _on_http_response(self, response):
         if not self.connect_future.done():
@@ -926,7 +926,7 @@ class WebSocketClientConnection(simple_httpclient._HTTPConnection):
             self._timeout = None
 
         self.stream = self.connection.detach()
-        self.stream.set_close_callback(self._on_close)
+        self.stream.set_close_callback(self.on_connection_close)
         # Once we've taken over the connection, clear the final callback
         # we set on the http request.  This deactivates the error handling
         # in simple_httpclient that would otherwise interfere with our
