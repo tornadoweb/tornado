dd9b2140e398276ba289b03fd6f40264c7810959
yang li <liyang1025@gmail.com>
2014-11-21 15:21:45 +0800

    Update blog.py
    if this global is close  or  down  create an new one

--- demos/blog/blog.py
+++ demos/blog/blog.py
@@ -66,7 +66,13 @@ class Application(tornado.web.Application):
 class BaseHandler(tornado.web.RequestHandler):
     @property
     def db(self):
-        return self.application.db
+        if self.application.db:
+            return self.application.db
+        else:
+            self.application.db= torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
+            
 
     def get_current_user(self):
         user_id = self.get_secure_cookie("blogdemo_user")
