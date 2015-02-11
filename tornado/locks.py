bcb4708f0260ebf71544cf612dc4bf6bd813aa7f
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-02-11 08:40:05 -0500

    Add __future__ imports to locks.py and condition_test.py.

--- tornado/locks.py
+++ tornado/locks.py
@@ -12,6 +12,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from __future__ import absolute_import, division, print_function, with_statement
+
 __all__ = ['Condition']
 
 import collections
--- tornado/test/condition_test.py
+++ tornado/test/condition_test.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, division, print_function, with_statement
+
 from datetime import timedelta
 import time
 import unittest
