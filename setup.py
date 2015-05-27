fdfaf3dffa49479c7461050eacca07bc5ee8d207
Ben Darnell <ben@bendarnell.com>
2015-05-26 21:46:05 -0400

    Set version number to 4.2.0

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ http://api.mongodb.org/python/current/installation.html#osx
 
 kwargs = {}
 
-version = "4.2b1"
+version = "4.2"
 
 with open('README.rst') as f:
     kwargs['long_description'] = f.read()
