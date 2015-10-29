147861f2687b46647fae6a2b7f584930154adafb
Neil Chintomby <nchintomby+github@gmail.com>
2015-10-29 16:33:30 -0700

    Use `127.0.0.1` when binding test server socket.

--- tornado/testing.py
+++ tornado/testing.py
@@ -97,8 +97,8 @@ def bind_unused_port(reuse_port=False):
 
     Returns a tuple (socket, port).
     """
-    [sock] = netutil.bind_sockets(None, 'localhost', family=socket.AF_INET,
-                                  reuse_port=reuse_port)
+    sock = netutil.bind_sockets(None, '127.0.0.1', family=socket.AF_INET,
+                                  reuse_port=reuse_port)[0]
     port = sock.getsockname()[1]
     return sock, port
 
