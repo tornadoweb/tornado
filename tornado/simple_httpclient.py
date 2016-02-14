641a28ad11729b8c929358da9242c13db63c62c7
Ben Darnell <ben@bendarnell.com>
2016-02-14 15:11:49 -0500

    Workaround conflict between new certifi and old openssl.
    Fixes #1534

--- tornado/simple_httpclient.py
+++ tornado/simple_httpclient.py
@@ -7,7 +7,7 @@ from tornado.httpclient import HTTPResponse, HTTPError, AsyncHTTPClient, main, _
 from tornado import httputil
 from tornado.http1connection import HTTP1Connection, HTTP1ConnectionParameters
 from tornado.iostream import StreamClosedError
-from tornado.netutil import Resolver, OverrideResolver, _client_ssl_defaults
+from tornado.netutil import Resolver, OverrideResolver, _client_ssl_defaults, _default_ca_certs
 from tornado.log import gen_log
 from tornado import stack_context
 from tornado.tcpclient import TCPClient
@@ -37,13 +37,6 @@ try:
     import certifi
 except ImportError:
     certifi = None
-
-
-def _default_ca_certs():
-    if certifi is None:
-        raise Exception("The 'certifi' package is required to use https "
-                        "in simple_httpclient")
-    return certifi.where()
 
 
 class SimpleAsyncHTTPClient(AsyncHTTPClient):
