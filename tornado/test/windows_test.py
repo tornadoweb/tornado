b49c87a715a87b2c31f4ba8e664379c783ff68cc
Ben Darnell <ben@bendarnell.com>
2016-05-21 20:23:51 -0400

    Fix error raised in set_close_exec on windows
    This slightly improves error messaging when tornado.process.Subprocess
    is used in windows.
    
    Fixes #1703

---
+++ tornado/test/windows_test.py
@@ -0,0 +1,25 @@
+import functools
+import os
+import socket
+import unittest
+
+from tornado.platform.auto import set_close_exec
+
+skipIfNonWindows = unittest.skipIf(os.name != 'nt', 'non-windows platform')
+
+@skipIfNonWindows
+class WindowsTest(unittest.TestCase):
+    def test_set_close_exec(self):
+        # set_close_exec works with sockets.
+        s = socket.socket()
+        self.addCleanup(s.close)
+        set_close_exec(s.fileno())
+
+        # But it doesn't work with pipes.
+        r, w = os.pipe()
+        self.addCleanup(functools.partial(os.close, r))
+        self.addCleanup(functools.partial(os.close, w))
+        with self.assertRaises(WindowsError) as cm:
+            set_close_exec(r)
+        ERROR_INVALID_HANDLE = 6
+        self.assertEqual(cm.exception.winerror, ERROR_INVALID_HANDLE)
