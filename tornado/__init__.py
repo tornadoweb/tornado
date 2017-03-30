50f1890e3e972eb49caebfe5b4fb5ef5fbfdcbc9
Ben Darnell <ben@bendarnell.com>
2017-03-30 09:17:36 -0400

    Version bump and release notes for 4.4.3

--- tornado/__init__.py
+++ tornado/__init__.py
@@ -25,5 +25,5 @@ from __future__ import absolute_import, division, print_function, with_statement
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version = "4.4.2"
-version_info = (4, 4, 2, 0)
+version = "4.4.3"
+version_info = (4, 4, 3, 0)
