e4f26acdabca32f6cc94a2681fb518dbce132688
Ben Darnell <ben@bendarnell.com>
2017-04-20 09:32:04 -0400

    Version 4.5.1

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ http://api.mongodb.org/python/current/installation.html#osx
 
 kwargs = {}
 
-version = "4.5"
+version = "4.5.1"
 
 with open('README.rst') as f:
     kwargs['long_description'] = f.read()
