e4c228caef1238a1ac837105c90df612831494d4
Steve <steve.liushihao@gmail.com>
2017-06-21 12:05:35 +0800

    more timeout because of NT

--- tornado/test/tcpclient_test.py
+++ tornado/test/tcpclient_test.py
@@ -162,17 +162,18 @@ class TCPClientTest(AsyncTestCase):
     @skipOnTravis
     @gen_test
     def test_connect_timeout(self):
-        timeout = 0.02
-        timeout_min = self.io_loop.time() + 0.01
-        timeout_max = self.io_loop.time() + 0.03
+        timeout = 0.05
+        timeout_min, timeout_max = 0.04, 0.10
 
         class TimeoutResolver(Resolver):
             def resolve(self, *args, **kwargs):
                 return Future()  # never completes
+        start_time = self.io_loop.time()
         with self.assertRaises(TimeoutError):
             yield TCPClient(resolver=TimeoutResolver()).connect(
                 '8.8.8.8', 12345, timeout=timeout)
-        self.assertTrue(timeout_min < self.io_loop.time() < timeout_max)
+        end_time = self.io_loop.time()
+        self.assertTrue(timeout_min < end_time - start_time < timeout_max)
 
 
 class TestConnectorSplit(unittest.TestCase):
