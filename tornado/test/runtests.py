1a9b9939376b80d9f053734f539b860e0dca1aff
Ben Darnell <ben@bendarnell.com>
2018-04-08 13:57:59 -0400

    test: Fix some rot in lesser-used test configs

--- tornado/test/runtests.py
+++ tornado/test/runtests.py
@@ -137,6 +137,8 @@ def main():
     # 2.7 and 3.2
     warnings.filterwarnings("ignore", category=DeprecationWarning,
                             message="Please use assert.* instead")
+    warnings.filterwarnings("ignore", category=PendingDeprecationWarning,
+                            message="Please use assert.* instead")
     # Twisted 15.0.0 triggers some warnings on py3 with -bb.
     warnings.filterwarnings("ignore", category=BytesWarning,
                             module=r"twisted\..*")
