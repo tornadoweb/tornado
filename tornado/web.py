db529031a1e1a6e951826aba0b7d0b18f05cd4c7
Garen Chan <1412950785@qq.com>
2018-11-03 03:40:33 +0800

    Fix #2521: Serve full file when range start is negative and exceeds the total length (#2522)
    * Serve full file while range negative past start.
    
    Fix issue #2521.
    
    * Return 416 while range end less than start.
    
    When range end less than start, the content length will be negative and
    HTTPOutputError will be raised.

--- tornado/web.py
+++ tornado/web.py
@@ -2600,16 +2600,24 @@ class StaticFileHandler(RequestHandler):
         size = self.get_content_size()
         if request_range:
             start, end = request_range
-            if (start is not None and start >= size) or end == 0:
+            if start is not None and start < 0:
+                start += size
+                if start < 0:
+                    start = 0
+            if (
+                start is not None
+                and (start >= size or (end is not None and start >= end))
+            ) or end == 0:
                 # As per RFC 2616 14.35.1, a range is not satisfiable only: if
                 # the first requested byte is equal to or greater than the
-                # content, or when a suffix with length 0 is specified
+                # content, or when a suffix with length 0 is specified.
+                # https://tools.ietf.org/html/rfc7233#section-2.1
+                # A byte-range-spec is invalid if the last-byte-pos value is present
+                # and less than the first-byte-pos.
                 self.set_status(416)  # Range Not Satisfiable
                 self.set_header("Content-Type", "text/plain")
                 self.set_header("Content-Range", "bytes */%s" % (size,))
                 return
-            if start is not None and start < 0:
-                start += size
             if end is not None and end > size:
                 # Clients sometimes blindly use a large range to limit their
                 # download size; cap the endpoint at the actual file size.
