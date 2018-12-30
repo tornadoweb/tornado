cb2d8977e53ad36eba730ebf9bd8d790cb835540
Ben Darnell <ben@bendarnell.com>
2018-12-29 19:55:27 -0500

    docs: Blacken conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import time
+
 sys.path.insert(0, os.path.abspath(".."))
 import tornado
 
@@ -20,8 +21,8 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
-primary_domain = 'py'
-default_role = 'py:obj'
+primary_domain = "py"
+default_role = "py:obj"
 
 autodoc_member_order = "bysource"
 autoclass_content = "both"
@@ -42,14 +43,12 @@ coverage_ignore_modules = [
 coverage_ignore_classes = [
     # tornado.gen
     "Runner",
-
     # tornado.web
     "ChunkedTransferEncoding",
     "GZipContentEncoding",
     "OutputTransform",
     "TemplateModule",
     "url",
-
     # tornado.websocket
     "WebSocketProtocol",
     "WebSocketProtocol13",
@@ -60,32 +59,36 @@ coverage_ignore_functions = [
     # various modules
     "doctests",
     "main",
-
     # tornado.escape
     # parse_qs_bytes should probably be documented but it's complicated by
     # having different implementations between py2 and py3.
     "parse_qs_bytes",
-
     # tornado.gen
     "Multi",
 ]
 
-html_favicon = 'favicon.ico'
+html_favicon = "favicon.ico"
 
 latex_documents = [
-    ('index', 'tornado.tex', 'Tornado Documentation', 'The Tornado Authors', 'manual', False),
+    (
+        "index",
+        "tornado.tex",
+        "Tornado Documentation",
+        "The Tornado Authors",
+        "manual",
+        False,
+    )
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3.6/', None),
-}
+intersphinx_mapping = {"python": ("https://docs.python.org/3.6/", None)}
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
 # On RTD we can't import sphinx_rtd_theme, but it will be applied by
 # default anyway.  This block will use the same theme when building locally
 # as on RTD.
 if not on_rtd:
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
+
+    html_theme = "sphinx_rtd_theme"
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
