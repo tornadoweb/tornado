99ea6d2cc908d69839270d2b8b65887945803b8b
Ben Darnell <ben@bendarnell.com>
2019-03-01 09:55:59 -0500

    Set version to 6.0

--- tornado/__init__.py
+++ tornado/__init__.py
@@ -22,5 +22,5 @@
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version = "6.0b1"
-version_info = (6, 0, 0, -98)
+version = "6.0"
+version_info = (6, 0, 0, 0)
