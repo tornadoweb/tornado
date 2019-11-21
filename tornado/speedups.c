8b231e0716cdb0099b5a96a9eb873f57eb095ba7
Michael V. DePalatis <mike@depalatis.net>
2019-11-21 12:31:55 -0700

    Remove legacy Python support in speedups.c

--- tornado/speedups.c
+++ tornado/speedups.c
@@ -56,7 +56,6 @@ static PyMethodDef methods[] = {
     {NULL, NULL, 0, NULL}
 };
 
-#if PY_MAJOR_VERSION >= 3
 static struct PyModuleDef speedupsmodule = {
    PyModuleDef_HEAD_INIT,
    "speedups",
@@ -69,9 +68,3 @@ PyMODINIT_FUNC
 PyInit_speedups(void) {
     return PyModule_Create(&speedupsmodule);
 }
-#else  // Python 2.x
-PyMODINIT_FUNC
-initspeedups(void) {
-    Py_InitModule("tornado.speedups", methods);
-}
-#endif
