a4d6d4826d06b1e41fd33265f4a390690db125ce
ElJefe <jeffgemail@gmail.com>
2020-04-03 13:34:42 -0400

    Check length for performance - should be 1

--- tornado/locale.py
+++ tornado/locale.py
@@ -248,9 +248,10 @@ class Locale(object):
                 return cls.get(code)
             if parts[0].lower() in _supported_locales:
                 return cls.get(parts[0].lower())
-            for supported in _supported_locales:
-                if code == supported.split("_")[0]:
-                    return cls.get(supported)
+            if len(parts) == 1:
+                for supported in _supported_locales:
+                    if code == supported.split("_")[0]:
+                        return cls.get(supported)
         return cls.get(_default_locale)
 
     @classmethod
