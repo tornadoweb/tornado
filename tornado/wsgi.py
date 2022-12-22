09095afff81accd447af26dad1fed202c8a7eae6
Grant Jenks <grant.jenks@gmail.com>
2022-12-22 10:29:58 -0800

    Add comment to docstring

--- tornado/wsgi.py
+++ tornado/wsgi.py
@@ -93,6 +93,26 @@ class WSGIContainer(object):
     The `tornado.web.FallbackHandler` class is often useful for mixing
     Tornado and WSGI apps in the same server.  See
     https://github.com/bdarnell/django-tornado-demo for a complete example.
+
+    `WSGIContainer` supports executing the WSGI application in custom executors
+    using `IOLoop.run_in_executor`. The default executor uses
+    `tornado.concurrent.dummy_executor` which works synchronously, but other
+    executors subclassing `concurrent.futures.Executor` may be used. To execute
+    WSGI application code in separate threads in an event-loop compatible way
+    use::
+
+        async def main():
+            executor = concurrent.futures.ThreadPoolExecutor()
+            # ^-- Execute requests in separate threads.
+            container = tornado.wsgi.WSGIContainer(simple_app, executor)
+            # Initialize the WSGI container with custom executor --^
+            http_server = tornado.httpserver.HTTPServer(container)
+            http_server.listen(8888)
+            await asyncio.Event().wait()
+
+    Running the WSGI app with a `ThreadPoolExecutor` remains *less scalable*
+    than running the same app in a multi-threaded WSGI server like ``gunicorn``
+    or ``uwsgi``.
     """
 
     def __init__(self, wsgi_application: "WSGIAppType", executor: futures.Executor = None) -> None:
