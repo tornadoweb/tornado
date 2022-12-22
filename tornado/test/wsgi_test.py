a60b0b2cfe54af382e00ef06e1b457c9e51dc459
Grant Jenks <grant.jenks@gmail.com>
2022-12-22 10:17:20 -0800

    Add test case for ThreadPoolExecutor usage

--- tornado/test/wsgi_test.py
+++ tornado/test/wsgi_test.py
@@ -1,19 +1,33 @@
+import concurrent.futures
+
 from wsgiref.validate import validator
 
 from tornado.testing import AsyncHTTPTestCase
 from tornado.wsgi import WSGIContainer
 
 
-class WSGIContainerTest(AsyncHTTPTestCase):
-    # TODO: Now that WSGIAdapter is gone, this is a pretty weak test.
+class WSGIAppMixin:
     def wsgi_app(self, environ, start_response):
         status = "200 OK"
         response_headers = [("Content-Type", "text/plain")]
         start_response(status, response_headers)
         return [b"Hello world!"]
 
+
+class WSGIContainerTest(WSGIAppMixin, AsyncHTTPTestCase):
+    # TODO: Now that WSGIAdapter is gone, this is a pretty weak test.
     def get_app(self):
         return WSGIContainer(validator(self.wsgi_app))
+
+    def test_simple(self):
+        response = self.fetch("/")
+        self.assertEqual(response.body, b"Hello world!")
+
+
+class WSGIContainerThreadPoolTest(WSGIAppMixin, AsyncHTTPTestCase):
+    def get_app(self):
+        executor = concurrent.futures.ThreadPoolExecutor()
+        return WSGIContainer(validator(self.wsgi_app), executor)
 
     def test_simple(self):
         response = self.fetch("/")
