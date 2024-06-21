5c2f7d7060af2420706da3e694c31c827a0071a3
devvmah <121362302+devvmah@users.noreply.github.com>
2024-06-21 19:21:55 +0200

    Update autoreload.py
    Branch coverage implementation

--- tornado/autoreload.py
+++ tornado/autoreload.py
@@ -110,6 +110,46 @@ _autoreload_is_main = False
 _original_argv: Optional[List[str]] = None
 _original_spec = None
 
+# Initialize coverage tracking global variable
+autoreload_start_dict = {
+    "start_1": False,  # Branch for checking if io_loop in _io_loops
+    "start_2": False,  # Branch for checking length of _io_loops > 1
+}
+autoreload_reload_on_update_dict = {
+    "reload_on_update_1": False,  # Branch for checking if _reload_attempted
+    "reload_on_update_2": False,  # Branch for checking process.task_id() is not None
+    "reload_on_update_3": False,  # Branch for checking if not isinstance(module, types.ModuleType)
+    "reload_on_update_4": False,  # Branch for checking if not path
+    "reload_on_update_5": False,  # Branch for checking if path.endswith(".pyc") or path.endswith(".pyo")
+}
+autoreload_check_file_dict = {
+    "check_file_1": False,  # Branch for checking if path not in modify_times
+    "check_file_2": False,  # Branch for checking if modify_times[path] != modified
+}
+autoreload_reload_dict = {
+    "reload_1": False,
+    "reload_2": False,
+    "reload_3": False,
+    "reload_4": False,
+    "reload_5": False,
+    "reload_6": False
+}
+autoreload_main_dict = {
+    "main_1": False,
+    "main_2": False,
+    "main_3": False,
+    "main_4": False,
+    "main_5": False,
+    "main_6": False,
+    "main_7": False,
+    "main_8": False,
+    "main_9": False,
+    "main_10 ": False,
+    "main_11": False,
+    "main_12": False,
+    "main_13": False, 
+    "main_14": False,
+}
 
 def start(check_time: int = 500) -> None:
     """Begins watching source files for changes.
@@ -119,14 +159,18 @@ def start(check_time: int = 500) -> None:
     """
     io_loop = ioloop.IOLoop.current()
     if io_loop in _io_loops:
+        autoreload_start_dict["start_1"] = True
         return
     _io_loops[io_loop] = True
     if len(_io_loops) > 1:
+        autoreload_start_dict["start_2"] = True
         gen_log.warning("tornado.autoreload started more than once in the same process")
     modify_times: Dict[str, float] = {}
     callback = functools.partial(_reload_on_update, modify_times)
     scheduler = ioloop.PeriodicCallback(callback, check_time)
     scheduler.start()
+    with open('autoreload_start.txt', 'a') as file:
+            file.write(str(autoreload_start_dict) + "\n")
 
 
 def wait() -> None:
@@ -161,9 +205,11 @@ def add_reload_hook(fn: Callable[[], None]) -> None:
 
 def _reload_on_update(modify_times: Dict[str, float]) -> None:
     if _reload_attempted:
+        autoreload_reload_on_update_dict["reload_on_update_1"] = True
         # We already tried to reload and it didn't work, so don't try again.
         return
     if process.task_id() is not None:
+        autoreload_reload_on_update_dict["reload_on_update_2"] = True
         # We're in a child process created by fork_processes.  If child
         # processes restarted themselves, they'd all restart and then
         # all call fork_processes again.
@@ -174,15 +220,20 @@ def _reload_on_update(modify_times: Dict[str, float]) -> None:
         # failures in getattr.  Just ignore anything that's not an ordinary
         # module.
         if not isinstance(module, types.ModuleType):
+            autoreload_reload_on_update_dict["reload_on_update_3"] = True
             continue
         path = getattr(module, "__file__", None)
         if not path:
+            autoreload_reload_on_update_dict["reload_on_update_4"] = True
             continue
         if path.endswith(".pyc") or path.endswith(".pyo"):
+            autoreload_reload_on_update_dict["reload_on_update_5"] = True
             path = path[:-1]
         _check_file(modify_times, path)
     for path in _watched_files:
         _check_file(modify_times, path)
+    with open('autoreload_reloadonupdate.txt', 'a') as file:
+            file.write(str(autoreload_reload_on_update_dict) + "\n")
 
 
 def _check_file(modify_times: Dict[str, float], path: str) -> None:
@@ -191,11 +242,15 @@ def _check_file(modify_times: Dict[str, float], path: str) -> None:
     except Exception:
         return
     if path not in modify_times:
+        autoreload_check_file_dict["check_file_1"] = True
         modify_times[path] = modified
         return
     if modify_times[path] != modified:
+        autoreload_check_file_dict["check_file_1"] = True
         gen_log.info("%s modified; restarting server", path)
         _reload()
+    with open('autoreload_checkfile.txt', 'a') as file:
+            file.write(str(autoreload_check_file_dict) + "\n")
 
 
 def _reload() -> None:
@@ -204,6 +259,7 @@ def _reload() -> None:
     for fn in _reload_hooks:
         fn()
     if sys.platform != "win32":
+        autoreload_reload_dict["reload_1"] = True
         # Clear the alarm signal set by
         # ioloop.set_blocking_log_threshold so it doesn't fire
         # after the exec.
@@ -213,13 +269,16 @@ def _reload() -> None:
     # change on re-exec.  Reconstruct the original command line to
     # ensure that the new process sees the same path we did.
     if _autoreload_is_main:
+        autoreload_reload_dict["reload_2"] = True
         assert _original_argv is not None
         spec = _original_spec
         argv = _original_argv
     else:
+        autoreload_reload_dict["reload_3"] = True
         spec = getattr(sys.modules["__main__"], "__spec__", None)
         argv = sys.argv
     if spec and spec.name != "__main__":
+        autoreload_reload_dict["reload_4"] = True
         # __spec__ is set in two cases: when running a module, and when running a directory. (when
         # running a file, there is no spec). In the former case, we must pass -m to maintain the
         # module-style behavior (setting sys.path), even though python stripped -m from its argv at
@@ -231,10 +290,15 @@ def _reload() -> None:
         argv = ["-m", spec.name] + argv[1:]
 
     if not _has_execv:
+        autoreload_reload_dict["reload_5"] = True
         subprocess.Popen([sys.executable] + argv)
         os._exit(0)
     else:
+        autoreload_reload_dict["reload_6"] = True
         os.execv(sys.executable, [sys.executable] + argv)
+
+    with open('autoreload_reload.txt', 'a') as file:
+            file.write(str(autoreload_reload_dict) + "\n")
 
 
 _USAGE = """
@@ -289,12 +353,15 @@ def main() -> None:
     )
     opts, rest = parser.parse_args()
     if opts.module is None:
+        autoreload_main_dict["main_1"] = True
         if not rest:
+            autoreload_main_dict["main_2"] = True
             print("Either -m or a path must be specified", file=sys.stderr)
             sys.exit(1)
         path = rest[0]
         sys.argv = rest[:]
     else:
+        autoreload_main_dict["main_3"] = True
         path = None
         sys.argv = [sys.argv[0]] + rest
 
@@ -302,17 +369,22 @@ def main() -> None:
     # All we care about is truthiness
     exit_status: Union[int, str, None] = 1
     try:
+        autoreload_main_dict["main_4"] = True
         import runpy
 
         if opts.module is not None:
+            autoreload_main_dict["main_5"] = True
             runpy.run_module(opts.module, run_name="__main__", alter_sys=True)
         else:
+            autoreload_main_dict["main_6"] = True
             assert path is not None
             runpy.run_path(path, run_name="__main__")
     except SystemExit as e:
+        autoreload_main_dict["main_7"] = True
         exit_status = e.code
         gen_log.info("Script exited with status %s", e.code)
     except Exception as e:
+        autoreload_main_dict["main_8"] = True
         gen_log.warning("Script exited with uncaught exception", exc_info=True)
         # If an exception occurred at import time, the file with the error
         # never made it into sys.modules and so we won't know to watch it.
@@ -321,28 +393,36 @@ def main() -> None:
         for filename, lineno, name, line in traceback.extract_tb(sys.exc_info()[2]):
             watch(filename)
         if isinstance(e, SyntaxError):
+            autoreload_main_dict["main_9"] = True
             # SyntaxErrors are special:  their innermost stack frame is fake
             # so extract_tb won't see it and we have to get the filename
             # from the exception object.
             if e.filename is not None:
+                autoreload_main_dict["main_10"] = True
                 watch(e.filename)
     else:
+        autoreload_main_dict["main_11"] = True
         exit_status = 0
         gen_log.info("Script exited normally")
     # restore sys.argv so subsequent executions will include autoreload
     sys.argv = original_argv
 
     if opts.module is not None:
+        autoreload_main_dict["main_12"] = True
         assert opts.module is not None
         # runpy did a fake import of the module as __main__, but now it's
         # no longer in sys.modules.  Figure out where it is and watch it.
         loader = pkgutil.get_loader(opts.module)
         if loader is not None and isinstance(loader, importlib.abc.FileLoader):
+            autoreload_main_dict["main_13"] = True
             watch(loader.get_filename())
     if opts.until_success and not exit_status:
+        autoreload_main_dict["main_14"] = True
         return
+    
+    with open('autoreload_main.txt', 'a') as file:
+            file.write(str(autoreload_main_dict) + "\n")
     wait()
-
 
 if __name__ == "__main__":
     # See also the other __main__ block at the top of the file, which modifies
