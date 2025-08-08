be9c374b2ea5e3d6c988c7ede8d284a649cfec37
Ben Darnell <ben@bendarnell.com>
2025-08-08 13:41:47 -0400

    Release notes for 6.5.2

--- tornado/__init__.py
+++ tornado/__init__.py
@@ -22,8 +22,8 @@
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version = "6.5.1"
-version_info = (6, 5, 1, 0)
+version = "6.5.2"
+version_info = (6, 5, 2, 0)
 
 import importlib
 import typing
