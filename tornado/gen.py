e0dfed82744fdbfd8781aba0934e403210af9415
AshwinDeshpandeBG <Ashwin.Deshpande@berkshiregrey.com>
2025-08-28 11:57:13 -0400

    initial

--- tornado/gen.py
+++ tornado/gen.py
@@ -221,7 +221,7 @@ def coroutine(
             result = ctx_run(func, *args, **kwargs)
         except (Return, StopIteration) as e:
             result = _value_from_stopiteration(e)
-        except Exception:
+        except (Exception, asyncio.CancelledError):
             future_set_exc_info(future, sys.exc_info())
             try:
                 return future
