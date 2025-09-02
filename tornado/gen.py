e2e836a141165af30de041254f47b0674c4f23fe
AshwinDeshpandeBG <Ashwin.Deshpande@berkshiregrey.com>
2025-09-02 13:21:22 -0400

    catch CancelledError in Generator section

--- tornado/gen.py
+++ tornado/gen.py
@@ -241,7 +241,7 @@ def coroutine(
                     future_set_result_unless_cancelled(
                         future, _value_from_stopiteration(e)
                     )
-                except Exception:
+                except (Exception, asyncio.CancelledError):
                     future_set_exc_info(future, sys.exc_info())
                 else:
                     # Provide strong references to Runner objects as long
